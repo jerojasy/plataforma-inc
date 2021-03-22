aeffdc5d9c6779805aa114b8c7200a5a6175129a
unknown <jaimee.rojas@hotmail.com>
2021-03-22 12:57:56 -0300

    Initial Commit

---
+++ www/js/scripts.js
@@ -0,0 +1,10 @@
+// script css test user incorrecto
+
+function alertDanger() {
+    var x = document.getElementById("alertrut");
+if (x.style.display === "none") {
+    x.style.display = "block";
+} else {
+    x.style.display = "none";
+}
+}
