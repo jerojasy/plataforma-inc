eabef7bd6d2b7d4b9dd202053980209cca9a5800
unknown <jaimee.rojas@hotmail.com>
2021-04-07 10:03:52 -0400

    se agrega dropdown a menu cursos, se agrega ventana de cursos registrados y cursos libres

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -19,6 +19,10 @@ function toggleDropDownAdmin() {
     element.classList.toggle("hidden");
 };
 
+function toggleDropDownCursos() {
+    var element = document.getElementById("dropCursos");
+    element.classList.toggle("hidden");
+};
 
 function openModal() {
 
@@ -57,3 +61,19 @@ function closeModalEdit() {
     modalBackEdit.classList.toggle("hidden");
     modalEdit.classList.toggle("hidden");
 }
+
+// funcion para tabs cursos
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace("bg-gray-200 border-l border-t border-r rounded-t text-gray-800 font-semibold", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += "bg-gray-200 border-l border-t border-r rounded-t text-gray-800 font-semibold";
+  }
